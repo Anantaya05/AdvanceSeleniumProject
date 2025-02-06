7cf600c8077629a3d361b006b1038f0b9181aa2d
admin <admin@DESKTOP-MVQ0I28>
2025-02-06 19:58:06 +0530

    Push1

---
+++ TrailProject/src/test/java/dummy/FunctionTest.java
@@ -0,0 +1,11 @@
+package dummy;
+
+import org.testng.annotations.Test;
+
+public class FunctionTest {
+	@Test
+	public void Test2() {
+		System.out.println("Hi");
+	}
+
+}
---
+++ TrailProject/src/test/java/dummy/MethodTest.java
@@ -0,0 +1,11 @@
+package dummy;
+
+import org.testng.annotations.Test;
+
+public class MethodTest {
+	@Test
+	public void test() {
+		System.out.println("Hello");
+	}
+
+}
